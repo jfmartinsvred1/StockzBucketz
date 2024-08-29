4eb3f031463cbd4d99c1d6ae9499adca9d6019c6
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-08-29 18:09:54 -0300

    env

--- src/FirebaseConfig.ts
+++ src/FirebaseConfig.ts
@@ -2,13 +2,13 @@ import * as firebase from 'firebase/app'
 import * as firebaseAuth from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAWielmTkYCbmzKVzouiJXyDxiieMbcvd4",
-    authDomain: "stockbucket-54bb9.firebaseapp.com",
-    projectId: "stockbucket-54bb9",
-    storageBucket: "stockbucket-54bb9.appspot.com",
-    messagingSenderId: "1081295465934",
-    appId: "1:1081295465934:web:4680074d2408d7f4914f9f",
-    measurementId: "G-4BJ06VJB80"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 
 const app = firebase.initializeApp(firebaseConfig);
